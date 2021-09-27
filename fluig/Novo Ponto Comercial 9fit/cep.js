cd824f6e2037e0cd789e193a27678dad2924bd80
arrivabenegustavo <arrivabenegu@gmail.com>
2021-09-27 16:06:34 -0300

    função para api viacep

---
+++ fluig/Novo Ponto Comercial 9fit/cep.js
@@ -0,0 +1,9 @@
+$("#cep").blur(function(){
+    $.getJSON("//viacep.com.br/ws/"+ $("#cep").val() + "/json/", function(dados){
+        $("#logradouro").val(dados.logradouro);
+        $("#bairro").val(dados.bairro);
+        $("#cidade").val(dados.localidade);
+        $("#estado").val(dados.uf);
+
+    })
+});
